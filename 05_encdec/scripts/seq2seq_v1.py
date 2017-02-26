cc6a9fd8c3efdb5d87152ccdf83d2e370d0050b8
saikrishna.lticmu <saikrishna.r@research.iiit.ac.in>
2017-02-25 22:02:40 -0500

    Batched

--- 05_encdec/scripts/seq2seq_v1.py
+++ 05_encdec/scripts/seq2seq_v1.py
@@ -275,6 +275,152 @@ class EncoderDecoder_debug:
 
 
 
+
+
+from collections import defaultdict
+from itertools import count
+import sys
+
+class RNNLanguageModel_batch:
+  
+    def __init__(self, model, LAYERS, INPUT_DIM, HIDDEN_DIM, VOCAB_SIZE, lookup, builder=SimpleRNNBuilder):
+        self.builder = builder(LAYERS, INPUT_DIM, HIDDEN_DIM, model)
+
+        self.lookup = lookup
+        self.R = model.add_parameters((VOCAB_SIZE, HIDDEN_DIM))
+        self.bias = model.add_parameters((VOCAB_SIZE))
+
+    def save_to_disk(self, filename):
+        model.save(filename, [self.builder, self.lookup, self.R, self.bias])
+
+    def load_from_disk(self, filename):
+        (self.builder, self.lookup, self.R, self.bias) = model.load(filename)
+        
+    def build_lm_graph(self, sent):
+        renew_cg()
+        init_state = self.builder.initial_state()
+
+        R = parameter(self.R)
+        bias = parameter(self.bias)
+        errs = [] # will hold expressions
+        es=[]
+        state = init_state
+        for (cw,nw) in zip(sent,sent[1:]):
+            # assume word is already a word-id
+            x_t = lookup(self.lookup, int(cw))
+            state = state.add_input(x_t)
+            y_t = state.output()
+            r_t = bias + (R * y_t)
+            err = pickneglogsoftmax(r_t, int(nw))
+            errs.append(err)
+        nerr = esum(errs)
+        return nerr
+
+    def get_loss_batch(self, sent_array):
+        renew_cg()
+        init_state = self.builder.initial_state()
+
+        R = parameter(self.R)
+        bias = parameter(self.bias)
+        wids = []
+        masks = []
+       
+
+        
+        # get the wids and masks for each step
+        # "I am good", "This is good", "Good Morning" -> [['I', 'Today', 'Good'], ['am', 'is', 'Morning'], ['good', 'good', '<S>'], ['I', 'Today', 'Good'], ['am', 'is', 'Morning'], ['good', 'good', '<S>']]
+
+        tot_words = 0
+        wids = []
+        masks = []
+        for i in range(len(sent_array[0])):
+           wids.append([
+                (sent[i] if len(sent)>i else 3) for sent in sent_array])
+           mask = [(1 if len(sent)>i else 0) for sent in sent_array]
+           masks.append(mask)
+           tot_words += sum(mask)
+           
+        # start the rnn by inputting "<s>"
+        init_ids = [2] * len(sent_array)
+        #print dy.lookup_batch(self.lookup,init_ids)
+        #print "Looked up"
+        s = init_state.add_input(dy.lookup_batch(self.lookup,init_ids))
+   
+        # feed word vectors into the RNN and predict the next word
+        losses = []
+        for wid, mask in zip(wids, masks):
+           # calculate the softmax and loss
+           #print "WID ", wid
+           score = dy.affine_transform([bias, R, s.output()])
+           loss = dy.pickneglogsoftmax_batch(score, wid)
+           # mask the loss if at least one sentence is shorter
+           if mask[-1] != 1:
+              mask_expr = dy.inputVector(mask)
+              mask_expr = dy.reshape(mask_expr, (1,), len(sent_array))
+              loss = loss * mask_expr
+           losses.append(loss)
+           # update the state of the RNN    
+           wemb = dy.lookup_batch(self.lookup, wid)
+           s = s.add_input(wemb) 
+  
+        return dy.sum_batches(dy.esum(losses)), tot_words       
+        
+        errs = [] # will hold expressions
+        es=[]
+       
+        for (wid, mask) in zip(wids, masks):
+            # assume word is already a word-id
+            x_t = lookup(self.lookup, int(cw))
+            state = state.add_input(x_t)
+            y_t = state.output()
+            r_t = bias + (R * y_t)
+            err = pickneglogsoftmax(r_t, int(nw))
+            errs.append(err)
+        nerr = esum(errs)
+        return nerr    
+    def predict_next_word(self, sentence):
+        renew_cg()
+        init_state = self.builder.initial_state()
+        R = parameter(self.R)
+        bias = parameter(self.bias)
+        state = init_state
+        for cw in sentence:
+            # assume word is already a word-id
+            x_t = lookup(self.lookup, int(cw))
+            state = state.add_input(x_t)
+        y_t = state.output()
+        r_t = bias + (R * y_t)
+        prob = softmax(r_t)
+        return prob
+    
+    def sample(self, first=1, nchars=0, stop=-1):
+        res = [first]
+        renew_cg()
+        state = self.builder.initial_state()
+
+        R = parameter(self.R)
+        bias = parameter(self.bias)
+        cw = first
+        while True:
+            x_t = lookup(self.lookup, cw)
+            state = state.add_input(x_t)
+            y_t = state.output()
+            r_t = bias + (R * y_t)
+            ydist = softmax(r_t)
+            dist = ydist.vec_value()
+            rnd = random.random()
+            for i,p in enumerate(dist):
+                rnd -= p
+                if rnd <= 0: break
+            res.append(i)
+            cw = i
+            if cw == stop: break
+            if nchars and len(res) > nchars: break
+        return res
+
+
+
+
 from collections import defaultdict
 from itertools import count
 import sys
